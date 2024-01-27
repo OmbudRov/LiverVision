a5a4b01758e00997da04a5e178c110a1ff78531b
OmbudRov <meatius2222@gmail.com>
2024-01-27 19:41:04 +0530

    Added more Utility Functions

--- Utilities/GenUtils.py
+++ Utilities/GenUtils.py
@@ -1,4 +1,6 @@
 import os 
+import tensorflow
+import cv2
 from glob import glob
 
 def CreateDirectory(Path):
@@ -8,5 +10,25 @@ def CreateDirectory(Path):
 def JoinPaths(*Paths):
     return os.path.normpath(os.path.sep.join(path.rstrip(r"\/") for path in Paths))
 
+def SetGPUs(GPU_IDs):
+    AllGPUs = tensorflow.config.experimental.list_physical_devices('GPU')
+    AllGPUsLength = len(AllGPUs)
+    if isinstance(GPU_IDs, int):
+        if GPU_IDs == -1:
+            GPU_IDs = range(AllGPUsLength)
+        else:
+            GPU_IDs = min(GPU_IDs, AllGPUsLength)
+            GPU_IDs = range(GPU_IDs)
+    
+    SelectedGPUs = [AllGPUs[GPU_ID] for GPU_ID in GPU_IDs if GPU_ID < AllGPUsLength]
+
+    try:
+        tensorflow.config.experimental.set_visible_devices(SelectedGPUs, 'GPU')
+    except RuntimeError as E:
+        print(E)
+
 def ResizeImage(Img, Height, Width, ResizeMethod = cv2.INTER_CUBIC):
     return cv2.resize(Img, dsize = (Width, Height), interpolation = ResizeMethod)
+
+def GetGPUsCount():
+    return len(tensorflow.config.experimental.list_logical_devices('GPU'))
